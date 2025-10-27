7d9fb0d6ebfef15273f12b2f68cc0a27a597a909
Noah Cylich <75380243+ncylich@users.noreply.github.com>
2025-10-26 21:53:09 -0700

    Insert debugging breakpoint in sampler.py
    Added a breakpoint for debugging purposes.

--- tunix/generate/sampler.py
+++ tunix/generate/sampler.py
@@ -454,6 +454,7 @@ class Sampler(base_sampler.BaseSampler):
           next_token_candidate
       )
 
+    import ipdb; ipdb.set_trace()
     done = done | jnp.isin(token_buffer[:, decoding_step + 1], eos)
     return _SamplingState(
         decoding_step=sampler_state.decoding_step + 1,
